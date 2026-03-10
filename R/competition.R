243b3eacfa3216dcb88bd33bcceb50db699f4653
Julian Grimm <grimmjulian225@gmail.com>
2026-03-10 14:51:52 +0100

    Initial commit

---
+++ R/competition.R
@@ -0,0 +1,72 @@
+parse_competition <- function(html) {
+	html <- rvest::read_html(html)
+	df <- html |> rvest::html_element(".Einzelnachweis") |> rvest::html_table()
+
+	df <- df |>
+		tidyr::fill(.data$X10) |>
+		dplyr::mutate(
+			home_team = df[[1]][[1]],
+			guest_team = df[[6]][[1]],
+			event = dplyr::recode_values(
+				.data$X10,
+				"Boden" ~ "floor",
+				"Pferd" ~ "pommel_horse",
+				"Ringe" ~ "still_rings",
+				"Sprung" ~ "vault",
+				"Barren" ~ "parallel_bars",
+				"Reck" ~ "high_bar"
+			),
+			event = factor(.data$event, levels = unique(.data$event)),
+			home_gymnast = .data$X1,
+			home_d_value = .data$X2,
+			home_end_value = .data$X3,
+			home_score_value = .data$X4,
+			guest_gymnast = .data$X6,
+			guest_d_value = .data$X7,
+			guest_end_value = .data$X8,
+			guest_score_value = .data$X9
+		)
+	event_start_row <- which(df[[1]] == "Turner") + 1
+	event_end_row <- which(df[[1]] == "Summe") - 1
+	event_rows <- Map(seq, event_start_row, event_end_row) |>
+		unlist()
+
+	df <- df[event_rows, ]
+	df <- cbind(df, parse_competition_tags(as.character(html)))
+
+	df |>
+		dplyr::group_by(.data$event) |>
+		dplyr::mutate(pairing_order = seq_len(dplyr::n())) |>
+		dplyr::ungroup() |>
+		dplyr::mutate(
+			dplyr::across(
+				dplyr::ends_with("value"),
+				\(x) as.numeric(gsub(",", ".", x))
+			)
+		) |>
+		dplyr::relocate(.data$home_gymnast_url, .after = .data$home_gymnast) |>
+		dplyr::relocate(.data$home_starts, .after = .data$home_gymnast_url) |>
+		dplyr::relocate(.data$guest_gymnast_url, .after = .data$guest_gymnast) |>
+		dplyr::select(-dplyr::starts_with("X"))
+}
+
+parse_competition_tags <- function(html) {
+	tags <- rvest::read_html(html) |>
+		rvest::html_element(".Einzelnachweis") |>
+		rvest::html_elements("a")
+
+	urls <- tags |>
+		rvest::html_attr("href")
+
+	is_starting <- tags |>
+		lapply(rvest::html_element, "span") |>
+		vapply(Negate(is.na), logical(1))
+
+	home_index <- seq(1, length(urls), by = 2)
+
+	data.frame(
+		home_gymnast_url = urls[home_index],
+		guest_gymnast_url = urls[home_index + 1],
+		home_starts = is_starting[home_index]
+	)
+}
---
+++ tests/testthat/test-competition.R
@@ -0,0 +1,75 @@
+test_that("parsing the competition returns a data frame", {
+	url <- testthat::test_path("competitions", "2900.html")
+	expect_true(is.data.frame(parse_competition(url)))
+})
+
+test_that("the right rows are kept", {
+	url <- testthat::test_path("competitions", "2900.html")
+	home_gymnast_col <- "home_gymnast"
+	df <- parse_competition(url)
+	vec <- df[[home_gymnast_col]]
+	expect_equal(vec[[1]], "Stanley, Jack")
+	expect_equal(vec[[5]], "Wagner, Aaron")
+	expect_equal(vec[[24]], "Prohorov, Nikita")
+	expect_equal(length(vec), 24)
+})
+
+test_that("teams are crawled correctly", {
+	url <- testthat::test_path("competitions", "2900.html")
+	df <- parse_competition(url)
+	expect_equal(df[["home_team"]][[1]], "KTV Koblenz")
+	expect_equal(df[["guest_team"]][[1]], "Exquisa Oberbayern")
+})
+
+test_that("events are parsed correctly", {
+	url <- testthat::test_path("competitions", "2900.html")
+	df <- parse_competition(url)
+	vec <- as.character(df[["event"]])
+	expect_equal(vec[[3]], "floor")
+	expect_equal(vec[[16]], "vault")
+})
+
+test_that("pairing order is parsed correctly", {
+	url <- testthat::test_path("competitions", "2900.html")
+	df <- parse_competition(url)
+	vec <- df[["pairing_order"]]
+	expect_equal(vec[1:4], 1:4)
+	expect_equal(vec[[5]], 1)
+})
+
+test_that("values are parsed correctly", {
+	url <- testthat::test_path("competitions", "2900.html")
+	df <- parse_competition(url)
+	expect_equal(df[["home_d_value"]][[1]], 5)
+	expect_equal(df[["home_end_value"]][[2]], 12.8)
+	expect_equal(df[["home_score_value"]][[3]], 5)
+	expect_equal(df[["guest_d_value"]][[4]], 4.1)
+	expect_equal(df[["guest_end_value"]][[5]], 12.05)
+	expect_equal(df[["guest_score_value"]][[6]], 0)
+	expect_false(any(is.na(df[["home_d_value"]])))
+	expect_false(any(is.na(df[["home_end_value"]])))
+	expect_false(any(is.na(df[["home_score_value"]])))
+	expect_false(any(is.na(df[["guest_d_value"]])))
+	expect_false(any(is.na(df[["guest_end_value"]])))
+	expect_false(any(is.na(df[["guest_score_value"]])))
+})
+
+test_that("starting gymnast is parsed correctly", {
+	url <- testthat::test_path("competitions", "2900.html")
+	df <- parse_competition_tags(url)
+	expect_false(df[["home_starts"]][[1]])
+	expect_true(df[["home_starts"]][[3]])
+})
+
+test_that("gymnast urls are parsed correctly", {
+	url <- testthat::test_path("competitions", "2900.html")
+	df <- parse_competition_tags(url)
+	expect_equal(
+		df[["home_gymnast_url"]][[7]],
+		"https://www.deutsche-turnliga.de/vereine/turner.html?ID=22318"
+	)
+	expect_equal(
+		df[["guest_gymnast_url"]][[8]],
+		"https://www.deutsche-turnliga.de/vereine/turner.html?ID=21198"
+	)
+})
