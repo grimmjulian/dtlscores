0aeb52fee8cbfc444243f9a1a7594cd1439cb784
Julian Grimm <grimmjulian225@gmail.com>
2026-03-18 11:29:18 +0100

    Use languageserver

--- R/competition.R
+++ R/competition.R
@@ -1,31 +1,31 @@
 fill <- function(vec) {
-	l <- seq_along(vec)
-	for (i in l[-1]) {
-		if (is.na(vec[[i]])) {
-			vec[[i]] <- vec[[i - 1]]
-		}
-	}
-	vec
+  l <- seq_along(vec)
+  for (i in l[-1]) {
+    if (is.na(vec[[i]])) {
+      vec[[i]] <- vec[[i - 1]]
+    }
+  }
+  vec
 }
 
 recode_events <- function(vec) {
-	fac <- factor(vec, levels = unique(vec))
-	levels(fac) <- c(
-		"floor",
-		"pommel_horse",
-		"still_rings",
-		"vault",
-		"parallel_bars",
-		"high_bar"
-	)
-	fac
+  fac <- factor(vec, levels = unique(vec))
+  levels(fac) <- c(
+    "floor",
+    "pommel_horse",
+    "still_rings",
+    "vault",
+    "parallel_bars",
+    "high_bar"
+  )
+  fac
 }
 
 relocate <- function(df, col, after) {
-	other_cols <- colnames(df)[which(!colnames(df) == col)]
-	pos <- match(after, other_cols)
-	cols <- append(other_cols, col, pos)
-	df[, cols]
+  other_cols <- colnames(df)[which(!colnames(df) == col)]
+  pos <- match(after, other_cols)
+  cols <- append(other_cols, col, pos)
+  df[, cols]
 }
 
 #' Parse competition results
@@ -39,74 +39,73 @@ relocate <- function(df, col, after) {
 #' \donttest{
 #' parse_competition("https://www.deutsche-turnliga.de/dtl/historie/archiv/detailsm0.html?ID=2787")
 #' }
-
 parse_competition <- function(url) {
-	url <- rvest::read_html(url)
-	df <- url |>
-		rvest::html_element(".Einzelnachweis") |>
-		rvest::html_table()
+  url <- rvest::read_html(url)
+  df <- url |>
+    rvest::html_element(".Einzelnachweis") |>
+    rvest::html_table()
 
-	df[["home_team"]] <- df[[1]][[1]]
-	df[["guest_team"]] <- df[[6]][[1]]
-	df[["event"]] <- df[["X10"]] |>
-		fill() |>
-		recode_events()
-	df[["home_gymnast"]] <- df[["X1"]]
-	df[["home_d_value"]] <- df[["X2"]]
-	df[["home_end_value"]] <- df[["X3"]]
-	df[["home_score_value"]] <- df[["X4"]]
-	df[["guest_gymnast"]] <- df[["X6"]]
-	df[["guest_d_value"]] <- df[["X7"]]
-	df[["guest_end_value"]] <- df[["X8"]]
-	df[["guest_score_value"]] <- df[["X9"]]
+  df[["home_team"]] <- df[[1]][[1]]
+  df[["guest_team"]] <- df[[6]][[1]]
+  df[["event"]] <- df[["X10"]] |>
+    fill() |>
+    recode_events()
+  df[["home_gymnast"]] <- df[["X1"]]
+  df[["home_d_value"]] <- df[["X2"]]
+  df[["home_end_value"]] <- df[["X3"]]
+  df[["home_score_value"]] <- df[["X4"]]
+  df[["guest_gymnast"]] <- df[["X6"]]
+  df[["guest_d_value"]] <- df[["X7"]]
+  df[["guest_end_value"]] <- df[["X8"]]
+  df[["guest_score_value"]] <- df[["X9"]]
 
-	event_start_row <- which(df[[1]] == "Turner") + 1
-	event_end_row <- which(df[[1]] == "Summe") - 1
-	event_rows <- Map(seq, event_start_row, event_end_row) |>
-		unlist()
+  event_start_row <- which(df[[1]] == "Turner") + 1
+  event_end_row <- which(df[[1]] == "Summe") - 1
+  event_rows <- Map(seq, event_start_row, event_end_row) |>
+    unlist()
 
-	df <- df[event_rows, ]
-	df <- cbind(df, parse_competition_tags(as.character(url)))
+  df <- df[event_rows, ]
+  df <- cbind(df, parse_competition_tags(as.character(url)))
 
-	for (e in levels(df[["event"]])) {
-		i <- df[["event"]] == e
-		df[i, "pairing_order"] <- seq_len(sum(i))
-	}
+  for (e in levels(df[["event"]])) {
+    i <- df[["event"]] == e
+    df[i, "pairing_order"] <- seq_len(sum(i))
+  }
 
-	value_cols <- endsWith(colnames(df), "value")
+  value_cols <- endsWith(colnames(df), "value")
 
-	for (v in which(value_cols)) {
-		df[[v]] <-
-			as.numeric(gsub(",", ".", df[[v]]))
-	}
+  for (v in which(value_cols)) {
+    df[[v]] <-
+      as.numeric(gsub(",", ".", df[[v]]))
+  }
 
-	df <- relocate(df, "home_gymnast_url", after = "home_gymnast")
-	df <- relocate(df, "home_starts", after = "home_gymnast_url")
-	df <- relocate(df, "guest_gymnast_url", after = "guest_gymnast")
-	df <- relocate(df, "pairing_order", after = "event")
+  df <- relocate(df, "home_gymnast_url", after = "home_gymnast")
+  df <- relocate(df, "home_starts", after = "home_gymnast_url")
+  df <- relocate(df, "guest_gymnast_url", after = "guest_gymnast")
+  df <- relocate(df, "pairing_order", after = "event")
 
-	x_cols <- startsWith(colnames(df), "X")
-	df[, !x_cols]
+  x_cols <- startsWith(colnames(df), "X")
+  df[, !x_cols]
 }
 
 parse_competition_tags <- function(html) {
-	tags <- rvest::read_html(html) |>
-		rvest::html_element(".Einzelnachweis") |>
-		rvest::html_elements("a")
+  tags <- rvest::read_html(html) |>
+    rvest::html_element(".Einzelnachweis") |>
+    rvest::html_elements("a")
 
-	urls <- tags |>
-		rvest::html_attr("href") |>
-		pad_urls()
+  urls <- tags |>
+    rvest::html_attr("href") |>
+    pad_urls()
 
-	is_starting <- tags |>
-		lapply(rvest::html_element, "span") |>
-		vapply(Negate(is.na), logical(1))
+  is_starting <- tags |>
+    lapply(rvest::html_element, "span") |>
+    vapply(Negate(is.na), logical(1))
 
-	home_index <- seq(1, length(urls), by = 2)
+  home_index <- seq(1, length(urls), by = 2)
 
-	data.frame(
-		home_gymnast_url = urls[home_index],
-		guest_gymnast_url = urls[home_index + 1],
-		home_starts = is_starting[home_index]
-	)
+  data.frame(
+    home_gymnast_url = urls[home_index],
+    guest_gymnast_url = urls[home_index + 1],
+    home_starts = is_starting[home_index]
+  )
 }
--- R/matchday.R
+++ R/matchday.R
@@ -9,62 +9,62 @@
 #' parse_matchday("https://www.deutsche-turnliga.de/archiv.html")
 #' }
 parse_matchday <- function(url) {
-	html <- rvest::read_html(url)
+  html <- rvest::read_html(url)
 
-	selection <- parse_selected(as.character(html))
+  selection <- parse_selected(as.character(html))
 
-	df <- selection |>
-		lapply(rep, times = 4) |>
-		as.data.frame()
+  df <- selection |>
+    lapply(rep, times = 4) |>
+    as.data.frame()
 
-	l <- html |>
-		rvest::html_element(".table-striped") |>
-		rvest::html_element("tbody") |>
-		rvest::html_elements("td")
+  l <- html |>
+    rvest::html_element(".table-striped") |>
+    rvest::html_element("tbody") |>
+    rvest::html_elements("td")
 
-	wettkampf <- l[seq(1, length(l), by = 3)]
-	score <- l[seq(2, length(l), by = 3)] |> rvest::html_text2()
-	gp <- l[seq(3, length(l), by = 3)] |> rvest::html_text2()
+  wettkampf <- l[seq(1, length(l), by = 3)]
+  score <- l[seq(2, length(l), by = 3)] |> rvest::html_text2()
+  gp <- l[seq(3, length(l), by = 3)] |> rvest::html_text2()
 
-	df[["datetime"]] <- wettkampf |>
-		rvest::html_text2() |>
-		strsplit(" Uhr ") |>
-		vapply(\(x) x[[1]], FUN.VALUE = character(1)) |>
-		as.POSIXct(format = "%d.%m.%Y %H:%M", tz = "Europe/Berlin")
+  df[["datetime"]] <- wettkampf |>
+    rvest::html_text2() |>
+    strsplit(" Uhr ") |>
+    vapply(\(x) x[[1]], FUN.VALUE = character(1)) |>
+    as.POSIXct(format = "%d.%m.%Y %H:%M", tz = "Europe/Berlin")
 
-	df[["location"]] <- wettkampf |>
-		as.character() |>
-		strsplit(" Uhr ") |>
-		vapply(\(x) x[[2]], FUN.VALUE = character(1)) |>
-		strsplit("<br>") |>
-		vapply(\(x) x[[1]], FUN.VALUE = character(1))
+  df[["location"]] <- wettkampf |>
+    as.character() |>
+    strsplit(" Uhr ") |>
+    vapply(\(x) x[[2]], FUN.VALUE = character(1)) |>
+    strsplit("<br>") |>
+    vapply(\(x) x[[1]], FUN.VALUE = character(1))
 
-	df[["title"]] <- wettkampf |>
-		rvest::html_element("a") |>
-		rvest::html_text2()
+  df[["title"]] <- wettkampf |>
+    rvest::html_element("a") |>
+    rvest::html_text2()
 
-	df[["competition_url"]] <- wettkampf |>
-		rvest::html_element("a") |>
-		rvest::html_attr("href") |>
-		pad_urls()
+  df[["competition_url"]] <- wettkampf |>
+    rvest::html_element("a") |>
+    rvest::html_attr("href") |>
+    pad_urls()
 
-	df[["score"]] <- score
-	df[["gp"]] <- gp
-	df
+  df[["score"]] <- score
+  df[["gp"]] <- gp
+  df
 }
 
 parse_selected <- function(html, name) {
-	xml <- rvest::read_html(html) |>
-		rvest::html_elements("select") |>
-		lapply(rvest::html_elements, "option[selected]")
-	type <- c("type", "season", "league", "matchday")
-	type_id <- paste(type, "id", sep = "_")
-	l <- list()
-	for (i in 1:4) {
-		l[[type_id[[i]]]] <- xml[[i]] |>
-			rvest::html_attr("value")
-		l[[type[[i]]]] <- xml[[i]] |>
-			rvest::html_text2()
-	}
-	l
+  xml <- rvest::read_html(html) |>
+    rvest::html_elements("select") |>
+    lapply(rvest::html_elements, "option[selected]")
+  type <- c("type", "season", "league", "matchday")
+  type_id <- paste(type, "id", sep = "_")
+  l <- list()
+  for (i in 1:4) {
+    l[[type_id[[i]]]] <- xml[[i]] |>
+      rvest::html_attr("value")
+    l[[type[[i]]]] <- xml[[i]] |>
+      rvest::html_text2()
+  }
+  l
 }
